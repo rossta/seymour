204de5c54cc3040664a8d60e94388f92cc53c34d
Ross Kaffenberger <rosskaff@gmail.com>
2014-05-21 17:53:02 -0400

    prepare for travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,15 +11,34 @@ require 'seymour/engine'
 require 'rspec/rails'
 require "ammeter/init"
 require 'factory_girl_rails'
+require 'database_cleaner'
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 Rails.backtrace_cleaner.remove_silencers!
-TestRedis::Server.start!(dir)
 
 RSpec.configure do |config|
   config.mock_with :rspec
   config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+    unless ENV['CI'] == 'travis'
+      TestRedis::Server.start!(dir)
+    end
+  end
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
 
